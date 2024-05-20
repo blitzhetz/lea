14f592845c06dc596576723c578c1a4ac0aef6b8
Slobodan Zivanovic <120286670+blitzhetz@users.noreply.github.com>
2024-05-20 22:59:43 +0200

    Added user functionality
    Added user functionality.
    Created FileManager class for creating file for user if it is not created.

--- src/main/java/com/blitzhetz/Main.java
+++ src/main/java/com/blitzhetz/Main.java
@@ -6,13 +6,10 @@ import com.blitzhetz.view.PasswordView;
 
 public class Main {
     public static void main(String[] args) {
-        PasswordModel model = new PasswordModel("passwords.txt");
+        PasswordModel model = new PasswordModel();
         PasswordView view = new PasswordView();
         PasswordController controller = new PasswordController(model, view);
 
-        controller.addPassword("gmail", "jakasifra123");
-        controller.addPassword("facebook", "slobodanz123");
-        controller.getPasswords();
-        controller.copyPassword("gmail");
+        controller.startApplication();
     }
 }
