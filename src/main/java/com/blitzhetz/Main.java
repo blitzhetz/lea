026ac1f2d73f57c4b60b2fa822de4c79ab085d28
Slobodan Zivanovic <120286670+blitzhetz@users.noreply.github.com>
2024-05-20 20:20:22 +0200

    Basic implementation
    Finished basic implementation.
    Removed mvnw stuff, I rly don't need it.
    Created script for running application.

--- src/main/java/com/blitzhetz/Main.java
+++ src/main/java/com/blitzhetz/Main.java
@@ -1,7 +1,18 @@
 package com.blitzhetz;
 
+import com.blitzhetz.controller.PasswordController;
+import com.blitzhetz.model.PasswordModel;
+import com.blitzhetz.view.PasswordView;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello there!");
+        PasswordModel model = new PasswordModel("passwords.txt");
+        PasswordView view = new PasswordView();
+        PasswordController controller = new PasswordController(model, view);
+
+        controller.addPassword("gmail", "jakasifra123");
+        controller.addPassword("facebook", "slobodanz123");
+        controller.getPasswords();
+        controller.copyPassword("gmail");
     }
 }
